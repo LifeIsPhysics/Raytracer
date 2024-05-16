763e9a3a95c4c389035d1103805930ec504a83af
bmert <berke.mert@tum.de>
2024-05-16 20:45:07 +0200

    Added antialiasing (aka edge smoothing) for rendering

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+
 
 // C++ STD redefinitions
 using std::make_shared;
@@ -18,6 +20,16 @@ const double pi  = 3.1415926535897932385;
 // Utility functions
 inline double degree_to_radians(double degree) {
   return degree * pi / 180.0;
+}
+
+inline double random_double() {
+  // Returns a random real in [0,1).
+  return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+  // Returns a random real in [min,max).
+  return min + (max - min) * random_double();
 }
 
 // Common headers
