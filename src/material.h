023f5d2e3571bb82e9dda233388c7d6b37f4d80a
bmert <berke.mert@tum.de>
2024-05-20 13:53:49 +0200

    Added Schlick's approximation to total reflection condition.

--- src/material.h
+++ src/material.h
@@ -78,7 +78,7 @@ public:
     vec3 direction;
     
     // For total internal reflection
-    if (cannot_refract)
+    if (cannot_refract || reflactance(cos_theta, ri) > random_double())
       direction = reflect(unit_direction, rec.normal);
     else
       direction = refract(unit_direction, rec.normal, ri);
@@ -91,6 +91,14 @@ private:
   // Refractive index in vacuum or air, or the ratio of the material's refractive index over
   // the refractive index of the enclosing media
   double _refraction_index;
+
+  static reflactance(double cosine, double refraction_index) {
+    // Use Schlick's approximation for reflactance.
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+  }
 };
 
 
